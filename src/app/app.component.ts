18f07af1700d7eb06f42414c27709b37b6ecb48d
YashPShah-swag <yash.pshah@softwareag.com>
2023-08-07 14:50:09 +0530

    closed/all added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,17 +58,22 @@ export class AppComponent{
   issues:any;
   issueList:any=[];
   pullList:any=[];
+  closedList:any=[];
+  allIssueList:any=[];
+  selectedView:string="Open";
   dataSource:any;
   lable_names: string[] = [];
   isLoading:boolean | undefined;
   pullCheck:boolean=false;
   progressValue:number=5;
   dataNotFound:boolean=false;
+  isErrorOccured:boolean=false;
+  errorMessage:string | undefined ;
   constructor(private http: HttpClient,public dialog: MatDialog){}
   displayedColumns = ['repository','issue_number', 'title','body','user_name','lable_name','created_at','updated_at'];
   accessKey="1617169566033";
-  prodEncryptURL="U2FsdGVkX1+VbkhbUk7Fy8RpMsXifJ7L9WAe6A8+LL61lCdV2wfUhjnDbS6FLd6iqTZ7Ad37SudeWoQr2RdvqrMZpRmva8MvDXjN3NrI5fA7akZC0ulpYI1jQFowiaC+08PbD74JfBfRjQWuBHJGvA==";
-  devEncryptURL="U2FsdGVkX1/xlJ2CJEZF8ziZEx18dp826fQSL3HFMVgbaZlw6wN9xsPWA7RY3Z2PYEyObKmvlpJqRAnNTue/QLSP90oSgXs6jE+QntjWtMuWi28HyjY4Xo+2/FEu5gf50M+S+BfRnJr10k62RV8GRg==";
+  devMainURL="U2FsdGVkX1/SY9wnkxUkrlQvGD7b9mxOqY9OatFNQLKMn/02eNQ2OrQu4yHgpqQMEeaPqHlRXjtc8iaaNSbN8MJA6BFhNqLuhde2Jg/gh6lv1495pohf9vEWb0Vkc2yt2hh2pMJIAYX4a+vm0Zl8vg=="
+  prodMainURL="U2FsdGVkX18utO43TS/VRwUZaz/p9Wl4OWmBP28z6p4O2SdCtZEH/+Zb7EGy05/zQmgmQu2MWkzxi/+dU0KYlaZrGlMh0ci6riHQhb5Wuk6Y+EEDLd1Fn6Ok3f4WfIB4"
   headers={'Contet-Type':'application/json'};
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!:MatSort;
@@ -78,63 +83,92 @@ export class AppComponent{
   selectedColumns(selected:string[]){
     this.displayedColumns=selected;
   }
-  getRepos(Repos: string){
+  
+  callAPI(Repos:string,Topic:string){
+    let prodURL=crypto.AES.decrypt(this.prodMainURL.toString(),this.accessKey).toString(crypto.enc.Utf8);
+    let devURL=crypto.AES.decrypt(this.devMainURL.toString(),this.accessKey).toString(crypto.enc.Utf8);
     this.submitClicked=true;
     this.isLoading=true;
     this.dataNotFound=false;
-    let ROOT_URL="";
-    if(isDevMode())
-      ROOT_URL=crypto.AES.decrypt(this.devEncryptURL.toString(),this.accessKey).toString(crypto.enc.Utf8)+'repository';
-    else
-      ROOT_URL=crypto.AES.decrypt(this.prodEncryptURL.toString(),this.accessKey).toString(crypto.enc.Utf8)+'repository';
-    this.progressValue=75;
-    this.http.get<JSON>(ROOT_URL,{headers:this.headers, params:{repos:Repos}}).subscribe((data)=>this.displayIssues(data));
-  }
-  getTopicIssues(Topic:string){
-    this.submitClicked=true;
-    this.isLoading=true;
-    this.dataNotFound=false;
-    let ROOT_URL="";
-    if(isDevMode())
-      ROOT_URL=crypto.AES.decrypt(this.devEncryptURL.toString(),this.accessKey).toString(crypto.enc.Utf8)+'TopicName';
-    else
-      ROOT_URL=crypto.AES.decrypt(this.prodEncryptURL.toString(),this.accessKey).toString(crypto.enc.Utf8)+'topicName';
-    this.progressValue=35;
-    setTimeout(() =>{this.progressValue=50},5000);
-    setTimeout(() =>{this.progressValue=75},10000);
-    setTimeout(() =>{this.progressValue=90},15000);
-    this.http.get<JSON>(ROOT_URL,{headers:this.headers, params:{topic:Topic}}).subscribe((data)=>this.displayIssues(data));
+    this.isErrorOccured=false;
+    if(Topic=='' || Topic==null){
+      this.progressValue=75;
+    }
+    else{
+      this.progressValue=35;
+      setTimeout(() =>{this.progressValue=50},5000);
+      setTimeout(() =>{this.progressValue=75},15000);
+      setTimeout(() =>{this.progressValue=90},20000);
+    }
+    if(isDevMode()){
+      this.http.get<JSON>(devURL,{headers:this.headers, params:{repos:Repos,topic:Topic}}).subscribe((data)=>this.displayIssues(data),(error)=>this.errorOccured(error));
+    }
+    else{
+      this.http.get<JSON>(prodURL,{headers:this.headers, params:{repos:Repos,topic:Topic}}).subscribe((data)=>this.displayIssues(data),(error)=>this.errorOccured(error));
+    }
   }
 
   displayIssues(data:any){
     this.pullList=[];
     this.issueList=[];
-    this.progressValue=90;
-    if(!(data.hasOwnProperty("finalOut"))){
-      this.dataNotFound=true;
-      this.progressValue=100;
-      this.isLoading=false;
-    }
-    this.issues=data.finalOut;
+    this.closedList=[];
+    this.allIssueList=[];
+    this.progressValue=90;  
+    this.issues=data.issues;
     let len=this.issues.length;
     for(let i=0;i<len;i++){
       if(!(this.issues[i].hasOwnProperty("lables"))){
         this.issues[i]["lables"]=[{"name":" "}];
       }
-      if(this.issues[i].user.type=='Bot'){
+      if(this.issues[i].user_type=='Bot'){
         this.pullList.push(this.issues[i]);
       }
-      if(!(this.issues[i].user.type=='Bot')){
+      else if(this.issues[i].status=='open'){
         this.issueList.push(this.issues[i]);
+      }
+      else if(this.issues[i].status=='closed'){
+        this.closedList.push(this.issues[i]);
       }
     }
     this.progressValue=100;
-    this.isLoading=false
+    this.isLoading=false;
     this.dataSource=new MatTableDataSource(this.issueList);
-    setTimeout(() =>{this.dataSource.paginator=this.paginator;},0);
-    this.dataSource.sort=this.sort;
+    setTimeout(() =>{this.dataSource.paginator=this.paginator; this.dataSource.sort=this.sort;},0);
   }
 
+  errorOccured(error:any){
+    if(error.status==404)
+      this.dataNotFound=true;
+    else
+      this.isErrorOccured=true;
+    this.errorMessage=error.error.error;
+    console.log("Error:",this.errorMessage);
+    this.progressValue=100;
+    this.isLoading=false;
+  }
+
+  changedView(){
+    if(this.selectedView=='Open'){
+      this.dataSource=new MatTableDataSource(this.issueList);
+      setTimeout(() =>{this.dataSource.paginator=this.paginator;},0);
+      this.dataSource.sort=this.sort;
+    }
+    if(this.selectedView=='Closed'){
+      this.dataSource=new MatTableDataSource(this.closedList);
+      setTimeout(() =>{this.dataSource.paginator=this.paginator;},0);
+      this.dataSource.sort=this.sort;
+    }
+    if(this.selectedView=='All'){
+      this.dataSource=new MatTableDataSource(this.issues);
+    setTimeout(() =>{this.dataSource.paginator=this.paginator;},0);
+    this.dataSource.sort=this.sort;
+    }
+    if(this.selectedView=='Pull'){
+      this.dataSource=new MatTableDataSource(this.pullList);
+    setTimeout(() =>{this.dataSource.paginator=this.paginator;},0);
+    this.dataSource.sort=this.sort;
+    }
+  }
   bodyDialog(cell:string){
     const bodyDialogRef=this.dialog.open(bodyDialogView,{width:'fit-content',height:'fit-content',data:cell});
   }
@@ -143,32 +177,12 @@ export class AppComponent{
     const titleDialogRef=this.dialog.open(titleDialogView,{width:'fit-content',height:'fit-content',data:cell});
   }
 
-  onClickedFilter(){
-    if(this.clickedFilter==true){
-      this.clickedFilter=false;
-    }
-    else{
-      this.clickedFilter=true;
-    }
-  }
+
 
   applyFilter(event: any) {
     this.dataSource.filter = event.target.value.trim().toLowerCase();
   }
-  changedRequest(){
-    if(this.pullCheck==false){
-      this.dataSource=new MatTableDataSource(this.pullList);
-      this.dataSource.paginator=this.paginator;
-      this.dataSource.sort=this.sort;
-      this.pullCheck=true;
-    }
-    else{
-      this.dataSource=new MatTableDataSource(this.issueList);
-      this.dataSource.paginator=this.paginator;
-      this.dataSource.sort=this.sort;
-      this.pullCheck=false;
-    }
-  }
+
 }
 
 @Component({
